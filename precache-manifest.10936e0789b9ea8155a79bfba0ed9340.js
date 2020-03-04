651e514dd5143fce5e647b5a35d894d68d5e405f
Oliver Inglis <ollie@theinglistwins.com>
2020-03-04 15:35:30 +0000

    Updates

---
+++ precache-manifest.10936e0789b9ea8155a79bfba0ed9340.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddd588596d502cb4959de5ed2a08af9e",
+    "url": "/react-food-delivery/index.html"
+  },
+  {
+    "revision": "e987d1ffe4eaa6c0c4f2",
+    "url": "/react-food-delivery/static/css/2.1fd2b905.chunk.css"
+  },
+  {
+    "revision": "0116898f98cd24a70251",
+    "url": "/react-food-delivery/static/css/main.cfe05538.chunk.css"
+  },
+  {
+    "revision": "e987d1ffe4eaa6c0c4f2",
+    "url": "/react-food-delivery/static/js/2.50d3926b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-food-delivery/static/js/2.50d3926b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0116898f98cd24a70251",
+    "url": "/react-food-delivery/static/js/main.044e4ec9.chunk.js"
+  },
+  {
+    "revision": "6299174558b681ff672b",
+    "url": "/react-food-delivery/static/js/runtime-main.2956b5f1.js"
+  }
+]);
